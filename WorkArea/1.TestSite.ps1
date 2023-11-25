eb1a05afa7b306f6ba7539a1c806200ea72be977
Conrad Gauntlett <conrad.gauntlett@awaze.com>
2023-11-25 19:36:57 +0000

    first commit

---
+++ WorkArea/1.TestSite.ps1
@@ -0,0 +1,18 @@
+Import-Module Pode, Pode.Web
+
+Start-PodeServer {
+
+    Add-PodeEndpoint -Address localhost -Port 8080 -Protocol Http -Force
+
+    New-PodeLoggingMethod -Terminal | Enable-PodeErrorLogging -Levels @("Error", "Warning")
+
+    Use-PodeWebTemplates -Title "Passwords" -Theme Dark
+
+    Add-PodeWebPage -Name "Test Page" -scriptblock {
+
+    }
+
+}
+
+git config --global user.email "conrad.gauntlett@awaze.com"
+  git config --global user.name "Conrad Gauntlett"
